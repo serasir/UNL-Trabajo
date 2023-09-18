a2dd2b5f38b1f44ced1646542aefe53c3549d5db
serasir <tomasmalisani@outlook.com>
2023-09-18 10:08:45 -0300

    Nuevo Enemigo

--- Prototipo/Assets/Scripts/Spawn.cs
+++ Prototipo/Assets/Scripts/Spawn.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
+    public GameObject[] PreFabs;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("Invocar",1,1.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void Invocar() 
+    {
+        Instantiate(PreFabs[0], transform.position, PreFabs[0].transform.rotation);
     }
 }
